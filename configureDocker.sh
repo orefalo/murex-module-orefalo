317704b4e9399dfe62a999be1e781a7e05b8a2f5
Olivier Refalo <orefalo@snapcore.com>
2023-08-14 23:54:19 +0200

    ch

--- configureDocker.sh
+++ configureDocker.sh
@@ -8,3 +8,7 @@ ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plu
 
 # osxkeychain and repo setup
 cp ./dockerconfig.json ~/.docker/config.json
+
+
+    alias start_minikube="colima start -cpu 4 -m 6 -k"
+    alias stop_minikube="colima stop"
