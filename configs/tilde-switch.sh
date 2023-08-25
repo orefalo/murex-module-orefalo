d865ffdb5f3ea926e28c3b1650cdad349fbb46b5
olivier <orefalo@gmail.com>
2023-08-25 15:38:57 +0400

    ch

--- configs/tilde-switch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
